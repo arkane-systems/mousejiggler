9ec2f000ea52e6263601e4e4358ea92f40b434f6
Robert <rroddy91@gmail.com>
2020-12-14 19:35:11 -0500

    added seconds parameter, informative notify tray text, and some code formatting cleanup

--- Program.cs
+++ Program.cs
@@ -13,87 +13,108 @@
 
 #region using
 
-using System ;
-using System.Reflection ;
-using System.Runtime.InteropServices ;
-using System.Threading ;
-using System.Windows.Forms ;
+using System;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Windows.Forms;
 
 #endregion
 
 namespace ArkaneSystems.MouseJiggle
 {
-    internal static class Program
-    {
-        private const int  AttachParentProcess = -1 ;
+	internal static class Program
+	{
+		private const int AttachParentProcess = -1;
+		private const int SecondsMinimum = 1;
+		private const int SecondsMaximum = 60;
 
-        public static bool StartJiggling ;
-        public static bool ZenJiggling ;
-        public static bool StartMinimized ;
+		public static bool StartJiggling;
+		public static bool ZenJiggling;
+		public static bool StartMinimized;
+		public static int StartWithSeconds;
 
+		private static readonly string[] JiggleCommands = { "-j", "--jiggle" };
+		private static readonly string[] MinimizeCommands = { "-m", "--minimized" };
+		private static readonly string[] ZenCommands = { "-z", "--zen" };
+		private static readonly string[] HelpCommands = { "-h", "--help" };
+		private static readonly string[] SecondsCommands = { "-s:", "--seconds:" };
 
-        // Required for attaching console output to the Windows Form Application
-        [DllImport ("kernel32.dll")]
-        private static extern bool AttachConsole (int dwProcessId) ;
+		// Required for attaching console output to the Windows Form Application
+		[DllImport("kernel32.dll")]
 
-        /// <summary>
-        ///     The main entry point for the application.
-        /// </summary>
-        [STAThread]
-        private static void Main (string[] args)
-        {
-            var instance = new Mutex (false, "single instance: ArkaneSystems.MouseJiggle") ;
+		private static extern bool AttachConsole(int dwProcessId);
 
-            if (instance.WaitOne (0, false))
-            {
-                // Check for command-line switches.
-                foreach (string rawarg in args)
-                {
-                    var arg = rawarg.ToUpperInvariant () ;
+		/// <summary>
+		///     The main entry point for the application.
+		/// </summary>
+		[STAThread]
+		private static void Main(string[] args)
+		{
+			var instance = new Mutex(false, "single instance: ArkaneSystems.MouseJiggle");
 
-                    if ((string.Compare (arg, "--JIGGLE", StringComparison.Ordinal) ==
-                         0) ||
-                        (string.Compare (arg, "-J", StringComparison.Ordinal) == 0))
-                        Program.StartJiggling = true ;
+			if (instance.WaitOne(0, false))
+			{
+				// Check for command-line switches. No need to loop through, just check for the specific allowed ones
+				if (JiggleCommands.Any(c => args.Contains(c, StringComparer.OrdinalIgnoreCase)))
+				{
+					StartJiggling = true;
+				}
+				if (ZenCommands.Any(c => args.Contains(c, StringComparer.OrdinalIgnoreCase)))
+				{
+					ZenJiggling = true;
+				}
+				if (HelpCommands.Any(c => args.Contains(c, StringComparer.OrdinalIgnoreCase)))
+				{
+					WriteHelpInfo();
+					return;
+				}
+				if (MinimizeCommands.Any(c => args.Contains(c, StringComparer.OrdinalIgnoreCase)))
+				{
+					StartMinimized = true;
+				}
 
-                    if ((string.Compare (arg, "--ZEN", StringComparison.Ordinal) == 0) ||
-                        (string.Compare (arg, "-Z",    StringComparison.Ordinal) == 0))
-                        Program.ZenJiggling = true ;
+				var secondsArg = args.FirstOrDefault(a => SecondsCommands.Any(c => a.StartsWith(c, StringComparison.OrdinalIgnoreCase)));
+				if (secondsArg != null)
+				{
+					var secondsValue =
+						secondsArg.Substring(secondsArg.IndexOf(":", StringComparison.InvariantCultureIgnoreCase) + 1);
+					if (!string.IsNullOrEmpty(secondsValue) && int.TryParse(secondsValue, out int _))
+					{
+						var seconds = int.Parse(secondsValue);
+						if (seconds < SecondsMinimum)
+						{
+							seconds = SecondsMinimum;
+						}
+						if (seconds > SecondsMaximum)
+						{
+							seconds = SecondsMaximum;
+						}
+						StartWithSeconds = seconds;
+					}
+				}
 
-                    if (
-                        (string.Compare (arg, "--MINIMIZED", StringComparison.Ordinal) ==
-                         0) ||
-                        (string.Compare (arg, "-M", StringComparison.Ordinal) == 0))
-                        Program.StartMinimized = true ;
-                    if (
-                        (string.Compare (arg, "--HELP", StringComparison.Ordinal) ==
-                         0) ||
-                        (string.Compare (arg, "-H", StringComparison.Ordinal) == 0))
-                    {
-                        Program.WriteHelpInfo () ;
-                        return ;
-                    }
-                }
+				Application.EnableVisualStyles();
+				Application.SetCompatibleTextRenderingDefault(false);
+				Application.Run(new MainForm());
+			}
 
-                Application.EnableVisualStyles () ;
-                Application.SetCompatibleTextRenderingDefault (false) ;
-                Application.Run (new MainForm ()) ;
-            }
+			instance.Close();
+		}
 
-            instance.Close () ;
-        }
-
-        private static void WriteHelpInfo ()
-        {
-            Program.AttachConsole (Program.AttachParentProcess) ;
-            Console.WriteLine ($@"
+		private static void WriteHelpInfo()
+		{
+			AttachConsole(AttachParentProcess);
+			Console.WriteLine($@"
 Mouse Jiggler {Assembly.GetEntryAssembly().GetName().Version}
 usage:
 
--z or --zen:        Start with zen (invisible) jiggling enabled
--j or --jiggle:     Start with jiggling enabled
--m or --minimized:  Start minimized
--h or --help:       Show help information") ;
-        }
-    }
+-z or --zen         Start with zen (invisible) jiggling enabled
+-j or --jiggle      Start with jiggling enabled
+-m or --minimized   Start minimized
+-s:X or --seconds:X Start with X seconds set for the jiggle interval
+-h or --help        Show help information");
+		}
+	}
 }
